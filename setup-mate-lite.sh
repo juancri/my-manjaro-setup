6c89c4aab67fe0109ac4d81ac8b215b951378bd3
JC Olivares <juancri@juancri.com>
2022-05-29 02:18:08 -0500

    Adding setup-mate-lite.sh

---
+++ setup-mate-lite.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Sync time
+echo "Syncing time..."
+sudo timedatectl set-ntp true
+
+# Find fastest mirror
+echo "Finding fastest mirror..."
+sudo pacman-mirrors --geoip
+
+# Update and upgrade
+echo "Update and upgrade..."
+sudo pacman -Syuu
+
+# Setup pikaur
+echo "Setting up pikaur..."
+sudo pacman -S --needed base-devel
+mkdir -p ~/src
+cd ~/src
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur
+makepkg -fsri
+cd ~
+
+# Don't compress packages
+echo "Setting pacman to don't compress packages..."
+sudo sed -i "s/PKGEXT='.pkg.tar.zst'/PKGEXT='.pkg.tar'/" /etc/makepkg.conf
+
+# Install extra packages
+echo "Installing extra packages..."
+pikaur -S \
+  bash-completion \
+  bash-git-prompt \
+  bind \
+  git-completion \
+  github-cli \
+  htop \
+  httpie \
+  jq \
+  lsd \
+  mc \
+  mlocate \
+  neovim \
+  net-tools \
+  synapse \
+  tldr \
+  traceroute \
+  visual-studio-code-bin \
+  whois
+
+# Install node
+echo "Installing nodejs..."
+mkdir -p ~/src/node
+pushd ~/src/node
+wget https://nodejs.org/dist/v16.14.0/node-v16.14.0-linux-x64.tar.xz
+tar xf node-v16.14.0-linux-x64.tar.xz
+popd
+
+# Display asterisks on sudo
+echo "Displaying asterisks on sudo..."
+sudo cp "${SCRIPT_DIR}/files/sudo" /etc/sudoers.d/00-asterisks
+
+# Install NvChad
+echo "Installing NvChad"
+git clone https://github.com/NvChad/NvChad ~/.config/nvim
+nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
+
+# Disable super key to open menu
+echo "Disabling super key as the shortcut to open the main menu..."
+gsettings set com.solus-project.brisk-menu hot-key ''
+
+# Set up bash
+echo "Setting up bash..."
+cp "${SCRIPT_DIR}/files/bashrc" ~/.bashrc
+
+# Generate SSH key
+echo "Generating SSH key..."
+mkdir -p ~/.ssh
+ssh-keygen -f ~/.ssh/id_rsa -N ""
+
+# Configure git
+echo "Configuring git..."
+git config --global init.defaultBranch master
+read -p "Enter your name: " FULLNAME
+read -p "Enter your email: " EMAIL
+git config --global user.name "${FULLNAME}"
+git config --global user.email "${EMAIL}"
+
+# Display public key
+echo "This is your public key. You can register it on GitHub."
+cat ~/.ssh/id_rsa.pub
